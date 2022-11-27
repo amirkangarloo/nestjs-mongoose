fb38798bf5e418d4aba609960880c0dfac0cd1af
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-11-27 15:49:07 +0330

    Implement schema and interface and create connection into mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { StudentSchema } from './schema';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017', {dbName: 'studentDB'}),
+    MongooseModule.forFeature([{name: 'student', schema: StudentSchema}])
+  ],
   controllers: [],
   providers: [],
 })
