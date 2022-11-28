296c2923d4e01bf90ae3b87f84b0530ef27a2527
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-11-28 08:58:25 +0330

    Implement .env file and use it

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StudentSchema } from './schema';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017', {dbName: 'studentDB'}),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME}),
     MongooseModule.forFeature([{name: 'student', schema: StudentSchema}])
   ],
   controllers: [],
