d5b3d8c694ed1972cccf8022bf3d8cccc6f5ec57
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-11-30 16:05:11 +0330

    Add swagger API document

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,14 @@ async function bootstrap() {
   app.enableVersioning({
     type: VersioningType.URI
   })
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs and mongoose')
+    .setDescription('CRUD with nestjs and mongoDB')
+    .setVersion('1.0')
+    .addTag('Students')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
   const port = process.env.APP_PORT || 3000;
   await app.listen(port);
 }
