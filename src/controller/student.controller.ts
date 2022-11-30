a998ea9e14fc90bd30acaff91c56088544761719
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-11-30 15:50:43 +0330

    Implement API versioning and complete student controller

--- src/controller/student.controller.ts
+++ src/controller/student.controller.ts
@@ -1,22 +1,33 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateStudentDto, UpdateStudentDto } from '../dto';
 import { StudentService } from '../service';
 
-@Controller('student')
+@Controller({ path: 'student', version: '1' })
 export class StudentController {
     constructor(private readonly studentService: StudentService) { }
     
     @Post()
-    public async createNewStudent() { }
+    public async createNewStudent(@Body() msg: CreateStudentDto) {
+        return await this.studentService.createStudent(msg);
+     }
     
     @Get()
-    public async getAllStudent() { }
+    public async getAllStudent() { 
+        return await this.studentService.getAllStudent();
+    }
     
     @Get('/:id')
-    public async getStudentById() { }
+    public async getStudentById(@Param() id: string) {
+        return await this.studentService.getStudentById(id);
+     }
     
     @Put('/:id')
-    public async updateStudentById() { }
+    public async updateStudentById(@Param() id: string, @Body() msg: UpdateStudentDto) {
+        return await this.studentService.updateStudentById(id, msg);
+     }
     
     @Delete('/:id')
-    public async deleteStudentById() { }
+    public async deleteStudentById(@Param() id: string) {
+        return await this.studentService.deleteStudentById(id);
+     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true, forbidNonWhitelisted: true}))
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
+  app.enableVersioning({
+    type: VersioningType.URI
+  })
+  const port = process.env.APP_PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
