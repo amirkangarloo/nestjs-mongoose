5b26b7dc769af72dffaf0d65778e680cb161ae40
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-12-02 09:42:16 +0330

    Implement ApiProperty with swagger

--- src/controller/student.controller.ts
+++ src/controller/student.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 import { CreateStudentDto, UpdateStudentDto } from '../dto';
 import { StudentService } from '../service';
 
@@ -7,26 +8,34 @@ export class StudentController {
     constructor(private readonly studentService: StudentService) { }
     
     @Post()
+    @ApiOperation({ summary: 'Create new student' })
     public async createNewStudent(@Body() msg: CreateStudentDto) {
         return await this.studentService.createStudent(msg);
      }
     
     @Get()
+    @ApiOperation({ summary: 'Get list of all students' })
     public async getAllStudent() { 
         return await this.studentService.getAllStudent();
     }
     
     @Get('/:id')
+    @ApiOperation({ summary: 'Get a student by id' })
+    @ApiParam({ name: 'id', type: 'string' })
     public async getStudentById(@Param() id: string) {
         return await this.studentService.getStudentById(id);
      }
     
     @Put('/:id')
+    @ApiOperation({ summary: 'Update a student by id' })
+    @ApiParam({ name: 'id', type: 'string' })
     public async updateStudentById(@Param() id: string, @Body() msg: UpdateStudentDto) {
         return await this.studentService.updateStudentById(id, msg);
      }
     
     @Delete('/:id')
+    @ApiParam({ name: 'id', type: 'string' })
+    @ApiOperation({ summary: 'Delete a student by id' })
     public async deleteStudentById(@Param() id: string) {
         return await this.studentService.deleteStudentById(id);
      }
--- src/dto/create-student.dto.ts
+++ src/dto/create-student.dto.ts
@@ -1,26 +1,55 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEnum, IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min } from "class-validator";
 import { GenderTypesEnum } from "../enum";
 
 
 export class CreateStudentDto {
     
+    @ApiProperty({
+        type: 'string',
+        required: true,
+        maxLength: 45,
+        example: 'Amir Kangarloo',
+    })
     @IsString()
     @IsNotEmpty()
     @MaxLength(45)
     readonly name: string;
 
+    @ApiProperty({
+        type: 'number',
+        required: true,
+        example: 1,
+    })
     @IsNumber()
     @IsNotEmpty()
     readonly roleNumber: number;
     
+    @ApiProperty({
+        type: 'number',
+        required: true,
+        example: 101,
+    })
     @IsNumber()
     @IsNotEmpty()
     readonly class: number;
     
+    @ApiProperty({
+        enum: GenderTypesEnum,
+        required: true,
+        example: 'mail',
+    })
     @IsEnum(GenderTypesEnum)
     @IsNotEmpty()
     readonly gender: string;
 
+    @ApiProperty({
+        type: 'number',
+        required: true,
+        maximum: 20,
+        minimum: 0,
+        example: 18,
+    })
     @IsNumber()
     @IsNotEmpty()
     @Min(0)
